4dacb0daaff06b4a72bff506d9c3498fb5351cff
Mayank Chopra <65212281+Mayank7832@users.noreply.github.com>
2021-02-25 15:41:41 +0530

    Add files via upload

---
+++ ui_aboutus.py
@@ -0,0 +1,45 @@
+import tkinter
+from tkinter import *
+from PIL import Image, ImageTk
+
+import matplotlib
+matplotlib.use('Agg')
+
+
+window = tkinter.Tk()
+
+window.geometry('900x650')
+window.title("GUI")
+window.configure(bg = 'gainsboro')
+window.resizable(width = False, height = False)
+
+load = Image.open("bg_img.jpeg")
+render = ImageTk.PhotoImage(load)
+
+img = Label(window, image=render)
+img.image = render
+img.place(x=0, y=0)
+
+label =  tkinter.Label(window,text = "About Us",font = ("Arial Bold",25))
+label.place(relx = 0.5, rely = 0.08, anchor ='n')
+
+label1 =  tkinter.Label(window,text = "Random text \n hsadfhsdihfspdfhsiodfhsdjfhsjdhf\njhdasgofjihgdsgjfhasdjkg",
+font = ("Arial Bold",22))
+label1.place(relx = 0.3, rely = 0.35, anchor ='n')
+
+
+
+button1=tkinter.Button(window, text="Start",font = ("Arial Bold",12),height = 2, width = 15)
+button1.place(x=625, y=200)
+
+button2=tkinter.Button(window, text="About",font = ("Arial Bold",12),height = 2, width = 15)
+button2.place(x=625, y=300)
+
+button3=tkinter.Button(window, text="Contact Us",font = ("Arial Bold",12),height = 2, width = 15)
+button3.place(x=625, y=400)
+
+button4=tkinter.Button(window, text="Exit",font = ("Arial Bold",12),height = 2, width = 15,command = window.destroy)
+button4.place(x=625, y=500)
+
+
+window.mainloop()
